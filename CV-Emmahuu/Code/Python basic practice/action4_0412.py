d5df074331acff9a1644323382a2e6ae1a84983a
Emmahuu424 <381554323@qq.com>
2020-04-12 12:52:56 +0800

    Triboracci Sequence

---
+++ CV-Emmahuu/Code/Python basic practice/action4_0412.py
@@ -0,0 +1,17 @@
+# list = [0,1,2,3,4]
+# list2 = list.copy()
+# del list2[3]
+# list.pop(1)
+# print(list)
+# print(list2)
+# list += [40, 50]
+# print(list)
+
+def tribonacci(signature, n):
+	for i in range(n)[3::]:
+		signature.append(sum(signature[i-3:i]))
+	print(signature[:n])
+
+tribonacci([1,1,1], 10)
+tribonacci([300,200,100], 0)
+tribonacci([0.5,0.5,0.5], 30)
