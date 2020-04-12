6574d147fd376f2d24e0083f594a68f13a3b3c18
Emmahuu424 <381554323@qq.com>
2020-04-12 18:15:20 +0800

    action1 sum(1,3,5...99) method2

--- CV-Emmahuu/Code/Python basic practice/action1_0409.py
+++ CV-Emmahuu/Code/Python basic practice/action1_0409.py
@@ -3,9 +3,16 @@ def sum(start, end, step):
 	print(result)
 
 sum(1, 99, 2)
+#16ms
 sum(1, 100, 1)
 sum(2, 100, 2)
 sum(1, 100, 3)
+
+sum2 = 0
+for i in range(1, 100, 2):
+	sum2 += i
+print(sum2)
+#20ms
 
 def end(start, step, times):
 	end_num = start + step * times
