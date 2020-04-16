90b6ae9efb3a951b93e52c351eaa92ffa62e7737
Emmahuu424 <381554323@qq.com>
2020-04-16 12:50:20 +0800

    action8 0416

--- CV-Emmahuu/Code/Python basic practice/action8_0416.py
+++ CV-Emmahuu/Code/Python basic practice/action8_0416.py
@@ -7,12 +7,6 @@
  """
 from datetime import datetime
 
-# str = '2020/04/16 20:00:00'
-# result = datetime.strptime(str, '%Y/%m/%d %H:%M:%S')
-# str2 = '2020/04/17 20:00:00'
-# result2 = datetime.strptime(str2, '%Y/%m/%d %H:%M:%S')
-# print(result)
-
 def check(enter_code, correct_code, current_date, expiration_date):
 	condition1 = enter_code == correct_code
 	condition2 =datetime.strptime(expiration_date, '%B %d, %Y') > datetime.strptime(current_date, '%B %d, %Y')
