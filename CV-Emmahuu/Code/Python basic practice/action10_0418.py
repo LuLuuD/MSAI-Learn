0034d5f0d36fcf7c6baf61f9e390767d8d46a646
Emmahuu424 <381554323@qq.com>
2020-04-19 19:05:16 +0800

    action11

--- CV-Emmahuu/Code/Python basic practice/action10_0418.py
+++ CV-Emmahuu/Code/Python basic practice/action10_0418.py
@@ -5,7 +5,6 @@
 """
 
  """
-import numpy as np
 def sort_by_name(arr):
 	num_to_19 = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen',
 	           'Seventeen','Eighteen','Nineteen']
@@ -29,11 +28,9 @@ def sort_by_name(arr):
 	for num in arr:
 		if len(str(num))//3 == 0:
 			new_arr.append(''.join(eng_name(num,'', 0)))
-			# new_arr[num] = ''.join(eng_name(num,'', 0))
 		else:
 			name_list = [eng_name((num//10**(3*i) % 10**3), '', i) for i in range(len(str(num))//3 -1 , -1, -1)]
 			new_arr.append(''.join(name_list))
-			# new_arr[num] = ''.join(name_list)
 	order = arr.sort(key = new_arr)
 	return order
 print(sort_by_name([8, 8, 9, 9, 10, 10]))
@@ -41,58 +38,3 @@ print(sort_by_name([1, 2, 3, 4]))
 print(sort_by_name([9, 99, 999]))
 
 
-
-
-
-# def sort_by_name2(arr):
-# 	num_to_19 = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen',
-# 	           'Seventeen','Eighteen','Nineteen']
-# 	num_to_90 = ['Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
-# 	unit = ['Billion','Million', 'Thousand']
-# 	def three_num(num):
-# 		H = num_to_19[num // 100] + ' Hundred'
-# 		T = num_to_90[num % 100//10 - 2] if num % 100//10 > 1 else ''
-# 		S = num_to_19[num % 10]  if num % 100 // 10 > 1 else num_to_19[num % 20]
-# 		result = H +' and ' + T + ' ' + S if num // 100 else T + ' ' + S
-# 		return result
-# 		# for i in num:
-# 		# 	if len(i) < 4:
-# 		# 		H = num_to_19[i//100] + 'Hundred'
-# 		# 		T = num_to_90[i%100-1%10]
-# 		# 		S = num_to_19[i%100]
-# 		# 		result = H + T + S if i//100 else T + S
-# 		# 		return result
-# 	result = []
-# 	number = ''
-# 	for i in arr:
-# 		if len(str(i)) < 4:
-# 			number = three_num(i)
-# 		else:
-# 			for time in range(len(str(i))//3):
-# 				# number += three_num(i//10**(3*(2-time))) +' ' +  unit[time] + ' and ' + three_num(i//10**(3*(2-time) -4))
-# 				number += three_num(i//10**(3*(2-time))) +' ' +  unit[time]
-# 		result.append(number)
-# 	print(1)
-# 	# def strin(x, y, z):
-# 	# 	if x < 10:
-# 	# 		num.append(x)
-# 	# 	num.append(strin(x%10, y + x%100 - x%10,z + x//100))
-# 	# [strin(i, 0, 0) for i in arr]
-# 	# 	return num
-# 	# number = [strin(i) for i in arr]
-# 	# def below100(num):
-# 	# 	for i in num:
-# 	# 		if i//10 <= 1:
-# 	# 			number = num_to_19[i]
-# 	# 		if len(i) < 3:
-# 	# 			number = num_to_90[i%100] if i//10 > 1 else num_to_19[i]
-# 	# 		else:
-# 	# 			number =num_to_19[i//100] + 'Hundred' +
-#
-# 	# def sort(string:str):
-# 	# 	circle = string
-# 	# if len(number) < 3:
-# 	# 	result = number
-
-
-# print(sort_by_name([8,8,9,9,10,10]))
