0e6772cabf0533ba8618b619de0bd4fdbfd1f0d5
Emmahuu424 <381554323@qq.com>
2020-04-19 18:58:33 +0800

    action11

--- CV-Emmahuu/Code/Python basic practice/action10_0418.py
+++ CV-Emmahuu/Code/Python basic practice/action10_0418.py
@@ -10,30 +10,35 @@ def sort_by_name(arr):
 	num_to_19 = ['Zero','One','Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Eleven','Twelve','Thirteen','Fourteen','Fifteen','Sixteen',
 	           'Seventeen','Eighteen','Nineteen']
 	num_to_90 = ['Twenty','Thirty','Forty','Fifty','Sixty','Seventy','Eighty','Ninety']
-	unit = [' ', ' ', ' Hundred And ','Thousand And ', 'Million And ','Billion And ']
-	new_arr = {}
+	unit = [' ', ' ', ' Hundred And ',' Thousand And ', ' Million And ',' Billion And ']
+	new_arr = []
+	def eng_name(n, result, time):
+		def double(n):
+			if n // 10 > 1:
+				double_nam = num_to_90[n // 10 -2] + unit[1] + num_to_19[n % 10]
+			else:
+				double_nam = num_to_19[n]
+			return double_nam
+		length = len(str(n))
+		if length == 3:
+			result += num_to_19[n // 100] + unit[2] + double(n % 100)
+		else:
+			result += double(n)
+		if time > 0: result += unit[2 + time]
+		return result
 	for num in arr:
-		def eng_name(number, result, time):
-			length = len(str(number))
-			for i in range(length):
-				n = number//10**(length -1  - i ) % 10
-				# if length == 2 and n == 1:
-				if i == 1 and n > 1:
-					result += num_to_90[n-2] + unit[2-i]
-				# elif i == 0 and length == 1:
-				# 	result += num_to_19[number]
-				else:
-					result += num_to_19[n] + unit[length -1  - i]
-			if time > 0: result += unit[2+ time]
-			return result
 		if len(str(num))//3 == 0:
-			new_arr[num] = ''.join(eng_name(num,'', 0))
+			new_arr.append(''.join(eng_name(num,'', 0)))
+			# new_arr[num] = ''.join(eng_name(num,'', 0))
 		else:
 			name_list = [eng_name((num//10**(3*i) % 10**3), '', i) for i in range(len(str(num))//3 -1 , -1, -1)]
-			new_arr[num] = ''.join(name_list)
-	order = new_arr.sort()
+			new_arr.append(''.join(name_list))
+			# new_arr[num] = ''.join(name_list)
+	order = arr.sort(key = new_arr)
 	return order
-print(sort_by_name([123558918, 3, 876,74,15]))
+print(sort_by_name([8, 8, 9, 9, 10, 10]))
+print(sort_by_name([1, 2, 3, 4]))
+print(sort_by_name([9, 99, 999]))
 
 
 
--- CV-Emmahuu/Code/Python basic practice/action11_0419.py
+++ CV-Emmahuu/Code/Python basic practice/action11_0419.py
@@ -10,5 +10,6 @@ def numJewelsInStones(J:str, S:str):
 	num = 0
 	matches = [num + len(re.findall(i, S)) for i in J]
 	return sum(matches)
+
 print(numJewelsInStones('aA','aAAbbbb'))
 print(numJewelsInStones('z','ZZ'))
