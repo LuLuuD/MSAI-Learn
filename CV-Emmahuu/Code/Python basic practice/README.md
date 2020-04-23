e7de5bf978dfbc6e7b755c6e6a8675013a55d245
Emmahuu424 <381554323@qq.com>
2020-04-23 17:32:55 +0800

    add action15

--- CV-Emmahuu/Code/Python basic practice/README.md
+++ CV-Emmahuu/Code/Python basic practice/README.md
@@ -1,4 +1,4 @@
-## Stack/Binary tree - action 15 
+## Heap/Binary tree - action 15 
 
 [Binary tree](https://www.cnblogs.com/chengxiao/p/6129630.html)
 
--- CV-Emmahuu/Code/Python basic practice/action15_0423.py
+++ CV-Emmahuu/Code/Python basic practice/action15_0423.py
@@ -6,7 +6,14 @@
 
  """
 import heapq
+import pandas as pd
+# nums = [12,8,10,5,36,4,32]
+# heapq.heapify(nums)
+# print(nums)
+# a = stock['shares'].values.tolist()
+# heapq.heapify(a)
+# print(a)
 
-nums = [12,8,10,5,36,4,32]
-heapq.heapify(nums)
-print(nums)
+stock = pd.DataFrame([[150, 165.51], [50, 174.79], [320, 19.63], [100, 121.15], [75, 1210.41]],columns=['shares', 'price'], index = ['MSFT', 'FB', 'YHOO', 'IBM', 'GOOG'])
+print('价格最高的两只股票：{}'.format([stock['price'].index.values[stock['price'] == i][0] for i in heapq.nlargest(2, stock['price'])]))
+print('买入shares最多的两只股票：{}'.format([stock['shares'].index.values[stock['shares'] == i][0] for i in heapq.nlargest(2, stock['shares'])]))
